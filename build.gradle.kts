aa62bf7c9ded9f9457dc104478f5da5a83593be5
HamzaAlfarrash <hamza.alfarrash@mail.mcgill.ca>
2026-02-14 22:17:52 -0500

    initial repo + ToDoUnitTest and session notes

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.rest-assured:rest-assured:5.4.0")
+    testImplementation("org.json:json:20240303")
+}
+
+sourceSets {
+    test {
+        java.srcDirs("unitTest")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
